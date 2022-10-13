284d8d0f735e01814e91132f41226a6ad6d20009
AlejandrodePablo <115726867+AlejandrodePablo@users.noreply.github.com>
2022-10-13 18:35:59 +0200

    Primer Proyecto

---
+++ src/PlacaBase.java
@@ -0,0 +1,25 @@
+public class MotherBoard {
+
+    private Integer id;
+    private String marca;
+    private String fabricante;
+
+    public.Integer getid();{
+        return.id;
+    }
+    public.void.id;{
+    }
+
+    public.String getmarca();{
+        return.marca
+    }
+    public.void.marca;{
+    }
+
+    public.String getfabricante();{
+        return.fabricante
+    }
+    public.void.fabricante;{
+
+    }
+}
---
+++ src/Procesador.java
@@ -0,0 +1,18 @@
+public class Procesador {
+
+    private Integer Id;
+    private String Marca;
+    private String Fabricante;
+
+    public.Integer getId();{
+        return.Id;
+    }
+    public.void.Id
+    public.String getMarca();{
+        return.Marca
+    }
+    public.String getFabricante();{
+        return.Fabricante
+    }
+
+}
